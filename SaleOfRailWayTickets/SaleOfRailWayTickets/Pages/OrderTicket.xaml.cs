986af2df3fa573f6a2552bc471f90300fbec701c
Stas <rozelstas@gmail.com>
2024-05-18 20:34:23 +0300

    add view for order ticket

---
+++ SaleOfRailWayTickets/SaleOfRailWayTickets/Pages/OrderTicket.xaml.cs
@@ -0,0 +1,39 @@
+﻿using lab4_5.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab4_5.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для OrderTicket.xaml
+    /// </summary>
+    public partial class OrderTicket : Page
+    {
+        public OrderTicket()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ClientWindow? window = App.Current.MainWindow as ClientWindow;
+
+            if (window != null)
+            {
+                window.MainFrame.Navigate(new PaymentByCard());
+            }
+        }
+    }
+}
--- SaleOfRailWayTickets/SaleOfRailWayTickets/ViewModel/UserViewModel.cs
+++ SaleOfRailWayTickets/SaleOfRailWayTickets/ViewModel/UserViewModel.cs
@@ -1,19 +1,25 @@
 ﻿using lab4_5.Classes;
 using lab4_5.View;
+using lab4_5.Pages;
 using Microsoft.VisualBasic.ApplicationServices;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
 
 namespace lab4_5.ViewModel
 {
-    public class UserViewModel
+    public class UserViewModel : INotifyPropertyChanged
     {
+        public TicketEssence selectedTicket;
+
         public ObservableCollection<TicketEssence>? Tickets { get; set; }
 
         public UnitWorkContent UnitWorkContent { get; set; }
@@ -84,10 +90,35 @@ namespace lab4_5.ViewModel
             }
         }
 
+        public TicketEssence SelectedTicket
+        {
+            get { return selectedTicket; }
+            set
+            {
+                selectedTicket = value;
+                if (SelectedTicket != null)
+                {
+                    ClientWindow window = App.Current.MainWindow as ClientWindow;
+                    if(window != null)
+                    {
+                        window.MainFrame.Navigate( new OrderTicket());
+                    }
+                }
+                OnPropertyChanged("SelectedTicket");
+            }
+        }
+
         public UserViewModel()
         {
             UnitWorkContent = new UnitWorkContent(new ApplicationDbContext());
             Tickets = new ObservableCollection<TicketEssence>(UnitWorkContent.TicketRepository.GetList());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
         }
     }
 }
